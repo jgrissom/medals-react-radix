674c08faa4ea7ecc82d71d19dc7b3ea1a41d28f4
Jeff Grissom <jgrissom91@gmail.com>
2024-09-10 20:08:17 -0500

    [2.08] Display Medal(s) in Table Body *** browser may warn about improper tbody nesting

--- src/components/Country.jsx
+++ src/components/Country.jsx
@@ -1,5 +1,5 @@
 import * as React from "react"
-// import Medal from "./Medal";
+import Medal from "./Medal";
 import { Box, Table, Flex, Badge, Button } from '@radix-ui/themes';
 import { TrashIcon } from '@radix-ui/react-icons';
 
@@ -25,6 +25,18 @@ function Country(props) {
             </Table.ColumnHeaderCell>
           </Table.Row>
         </Table.Header>
+
+        <Table.Body>
+          {props.medals.sort((a, b) => a.rank - b.rank).map(medal =>
+            <Medal
+              key={medal.id}
+              medal={medal}
+              country={props.country}
+              onIncrement={props.onIncrement}
+              onDecrement={props.onDecrement}
+            />
+          )}
+        </Table.Body>
       </Table.Root>
     </Box>
   )
