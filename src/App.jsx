36706cd229da8fe75c712741f15ac1a9bc38c408
jgrissom <jgrissom91@gmail.com>
2024-09-16 20:29:05 -0500

    [3.02] Fetch data using Axios - Import Axios into App component - Asynchronously load countries into app state

--- src/App.jsx
+++ src/App.jsx
@@ -1,42 +1,64 @@
-import { useState, useEffect, useRef } from 'react'
-import NewCountry from './components/NewCountry';
-import Country from './components/Country';
-import { Theme, Button, Flex, Heading, Badge, Container } from '@radix-ui/themes';
-import { SunIcon, MoonIcon } from '@radix-ui/react-icons';
-import '@radix-ui/themes/styles.css';
-import './App.css'
+import { useState, useEffect, useRef } from "react";
+import axios from "axios";
+import NewCountry from "./components/NewCountry";
+import Country from "./components/Country";
+import {
+  Theme,
+  Button,
+  Flex,
+  Heading,
+  Badge,
+  Container,
+} from "@radix-ui/themes";
+import { SunIcon, MoonIcon } from "@radix-ui/react-icons";
+import "@radix-ui/themes/styles.css";
+import "./App.css";
 
 function App() {
   const [appearance, setAppearance] = useState("dark");
   const [countries, setCountries] = useState([]);
   const medals = useRef([
-    { id: 1, name: 'gold', color: '#FFD700', rank: 1 },
-    { id: 2, name: 'silver', color: '#C0C0C0', rank: 2 },
-    { id: 3, name: 'bronze', color: '#CD7F32', rank: 3 },
+    { id: 1, name: "gold", color: "#FFD700", rank: 1 },
+    { id: 2, name: "silver", color: "#C0C0C0", rank: 2 },
+    { id: 3, name: "bronze", color: "#CD7F32", rank: 3 },
   ]);
+  const apiEndpoint = "https://medalsapi.azurewebsites.net/api/country";
 
   function handleAdd(name) {
-    const id = countries.length === 0 ? 1 : Math.max(...countries.map(country => country.id)) + 1;
-    setCountries([...countries].concat({ id: id, name: name, gold: 0, silver: 0, bronze: 0 }));
+    // const id =
+    //   countries.length === 0
+    //     ? 1
+    //     : Math.max(...countries.map((country) => country.id)) + 1;
+    // setCountries(
+    //   [...countries].concat({
+    //     id: id,
+    //     name: name,
+    //     gold: 0,
+    //     silver: 0,
+    //     bronze: 0,
+    //   })
+    // );
   }
   function handleDelete(countryId) {
-    setCountries([...countries].filter(c => c.id !== countryId));
+    // setCountries([...countries].filter((c) => c.id !== countryId));
   }
   function handleIncrement(countryId, medalName) {
-    const idx = countries.findIndex(c => c.id === countryId);
+    const idx = countries.findIndex((c) => c.id === countryId);
     const mutableCountries = [...countries];
     mutableCountries[idx][medalName] += 1;
     setCountries(mutableCountries);
   }
   function handleDecrement(countryId, medalName) {
-    const idx = countries.findIndex(c => c.id === countryId);
+    const idx = countries.findIndex((c) => c.id === countryId);
     const mutableCountries = [...countries];
     mutableCountries[idx][medalName] -= 1;
     setCountries(mutableCountries);
   }
   function getAllMedalsTotal() {
     let sum = 0;
-    medals.current.forEach(medal => { sum += countries.reduce((a, b) => a + b[medal.name], 0); });
+    medals.current.forEach((medal) => {
+      sum += countries.reduce((a, b) => a + b[medal.name], 0);
+    });
     return sum;
   }
   function toggleAppearance() {
@@ -46,30 +68,21 @@ function App() {
   // this is the functional equivalent to componentDidMount
   useEffect(() => {
     // initial data loaded here
-    let fetchedCountries = [
-      { id: 1, name: 'United States', gold: 2, silver: 2, bronze: 3 },
-      { id: 2, name: 'China', gold: 3, silver: 1, bronze: 0 },
-      { id: 3, name: 'Germany', gold: 0, silver: 2, bronze: 2 },
-      { id: 4, name: 'France', gold: 2, silver: 2, bronze: 1 },
-      { id: 5, name: 'Spain', gold: 1, silver: 1, bronze: 0 },
-      { id: 6, name: 'United Kingdom', gold: 0, silver: 2, bronze: 3 },
-      { id: 7, name: 'Brazil', gold: 3, silver: 0, bronze: 0 },
-      { id: 8, name: 'Italy', gold: 2, silver: 2, bronze: 2 },
-      { id: 9, name: 'Switzerland', gold: 1, silver: 1, bronze: 2 },
-      { id: 10, name: 'Poland', gold: 0, silver: 2, bronze: 1 },
-      { id: 11, name: 'Sweden', gold: 0, silver: 3, bronze: 1 },
-      { id: 12, name: 'Ireland', gold: 2, silver: 1, bronze: 0 },
-      { id: 13, name: 'Scotland', gold: 3, silver: 0, bronze: 2 },
-    ]
-    setCountries(fetchedCountries);
+    async function fetchCountries() {
+      const { data: fetchedCountries } = await axios.get(apiEndpoint);
+      setCountries(fetchedCountries);
+    }
+    fetchCountries();
   }, []);
 
   return (
     <Theme appearance={appearance}>
-      <Button onClick={toggleAppearance} style={{ position: "fixed", bottom: 20, right: 20, zIndex: 100 }} variant="ghost">
-        {
-          (appearance === "dark") ? <MoonIcon /> : <SunIcon />
-        }
+      <Button
+        onClick={toggleAppearance}
+        style={{ position: "fixed", bottom: 20, right: 20, zIndex: 100 }}
+        variant="ghost"
+      >
+        {appearance === "dark" ? <MoonIcon /> : <SunIcon />}
       </Button>
       <Flex p="2" pl="8" className="fixedHeader" justify="between">
         <Heading size="6">
@@ -80,11 +93,11 @@ function App() {
         </Heading>
         <NewCountry onAdd={handleAdd} />
       </Flex>
-      <Container className="bg">
-      </Container>
+      <Container className="bg"></Container>
       <Flex wrap="wrap" justify="center">
-        {
-          countries.sort((a, b) => a.name.localeCompare(b.name)).map(country =>
+        {countries
+          .sort((a, b) => a.name.localeCompare(b.name))
+          .map((country) => (
             <Country
               key={country.id}
               country={country}
@@ -93,11 +106,10 @@ function App() {
               onIncrement={handleIncrement}
               onDecrement={handleDecrement}
             />
-          )
-        }
+          ))}
       </Flex>
     </Theme>
-  )
+  );
 }
 
-export default App
+export default App;
