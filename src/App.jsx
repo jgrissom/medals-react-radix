d0a129127978b87dbaffdd7d598f7f17cdeefe00
jgrissom <jgrissom91@gmail.com>
2024-10-05 17:23:19 -0500

    [6.06] Handle Login 🚀 🐧 Axios Post

--- src/App.jsx
+++ src/App.jsx
@@ -32,6 +32,7 @@ function App() {
   // const apiEndpoint = "https://medalsapi.azurewebsites.net/api/country";
   const apiEndpoint = "https://medalsapi.azurewebsites.net/jwtapi/country";
   const hubEndpoint = "https://medalsapi.azurewebsites.net/medalsHub";
+  const userEndpoint = "https://jwtswagger.azurewebsites.net/api/user/login";
 
   async function handleAdd(name) {
     try {
@@ -142,8 +143,26 @@ function App() {
     });
     setCountries(mutableCountries);
   }
-  function handleLogin(username, password) {
-    console.log(`username: ${username}, password: ${password}`);
+  async function handleLogin(username, password) {
+    try {
+      const resp = await axios.post(userEndpoint, {
+        username: username,
+        password: password,
+      });
+      const encodedJwt = resp.data.token;
+      console.log(encodedJwt);
+    } catch (ex) {
+      if (
+        ex.response &&
+        (ex.response.status === 401 || ex.response.status === 400)
+      ) {
+        alert("Login failed");
+      } else if (ex.response) {
+        console.log(ex.response);
+      } else {
+        console.log("Request failed");
+      }
+    }
   }
   function getAllMedalsTotal() {
     let sum = 0;
