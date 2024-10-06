50171d3e85ecd29a368dd2c02cd90f090091f451
jgrissom <jgrissom91@gmail.com>
2024-10-05 19:25:47 -0500

    🚀🚀🚀 deploy 🚀🚀🚀 npm run build

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/aws-apps/medals-radix-05/",
+  base: "/aws-apps/medals-radix-06/",
   plugins: [react()],
 });
